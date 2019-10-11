108267a8fb511f2f3edde6d04a7fd2351a9f0f82
Steven Lipton <steve@makeapppie.com>
2019-10-11 05:32:32 -0500

    Initial Commit

---
+++ settersGetters.playground/Contents.swift
@@ -0,0 +1,25 @@
+//
+//  A Demo for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
+
+import Foundation
+
+
+///Figures the volume and area of pizzas
+class Pizza{
+    var size:Double = 10.0 //A diameter
+    var radius:Double
+    var area:Double
+}
+
+
+let pizza = Pizza()
+pizza.size = 12.0
+pizza.radius
+pizza.area
+
+pizza.area = 80
+pizza.radius
+pizza.size
